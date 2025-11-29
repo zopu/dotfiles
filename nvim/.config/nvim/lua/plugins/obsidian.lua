e6c9373ae195fe327041270e6a3c67b01c1ed228
Mike Perrow <mike.perrow@gmail.com>
2025-11-29 16:27:09 +0000

    neovim: add obsidian

---
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,21 @@
+return {
+  "epwalsh/obsidian.nvim",
+  version = "*",
+  lazy = true,
+  ft = "markdown",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    workspaces = {
+      {
+        name = "personal",
+        path = "~/vaults/personal",
+      },
+      {
+        name = "work",
+        path = "~/vaults/Ivi",
+      },
+    },
+  },
+}
